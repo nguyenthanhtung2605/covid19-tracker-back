f16fe1ecc313c4beb83276bae649e381f5607270
Tung Nguyen <nguyenthanhtung2605@gmail.com>
2020-04-14 15:53:28 -0400

    first commit

---
+++ covid19-tracker-back/src/main/java/io/soen487p3/webservice/covid19tracker/Covid19trackerApplication.java
@@ -0,0 +1,13 @@
+package io.soen487p3.webservice.covid19tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19trackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19trackerApplication.class, args);
+	}
+
+}
---
+++ covid19-tracker-back/src/test/java/io/soen487p3/webservice/covid19tracker/Covid19trackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.soen487p3.webservice.covid19tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19trackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
